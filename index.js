e3d0951ba7ba2d3c990b16e1334e616e70d820ab
Valeriy Skakun <valor220@gmail.com>
2022-01-03 19:52:34 +0200

    feat: add navigation menu

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log();
+const navigation = document.getElementById('navigation');
+
+document.addEventListener('click', (e) => {
+  if (
+    e.target.classList.contains('navigation-toggle') ||
+    e.target.classList.contains('navigation-toggle-img')
+  ) {
+    navigation.classList.toggle('navigation-hide');
+  } else {
+    navigation.classList.add('navigation-hide');
+  }
+});
